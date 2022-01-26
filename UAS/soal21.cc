c6866482c4030cfd45f1407ed2bcd9b14ae85b50
root <root@LAPTOP-0LPR93CT.localdomain>
2022-01-26 22:38:23 +0700

    fresh from oven

---
+++ UAS/soal21.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void nama(){
+    string nama;
+    cout << "Masukan Nama : ";
+    getline(cin, nama);
+    cout << "Nama : " << nama << endl;
+};
+
+void nim(){
+    string nim;
+    cout << "Masukan NIM : ";
+    cin>>nim;
+    cout << "NIm : " << nim << endl;
+};
+
+int main(){
+    nama();
+    nim();
+   return 0;
+}
